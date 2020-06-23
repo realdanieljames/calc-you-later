1cf5bf02c3b30825f1087e633fcadc3a417ca74c
DANIEL JAMES <daniel.james@codeimmersives.com>
2020-06-23 14:36:55 -0400

    user can now enter their calculation using the syntax: [3 + 3] and get an answer, NOT [3 3 +] like before.

--- calculate-front-end.js
+++ calculate-front-end.js
@@ -2,10 +2,15 @@
  * YOUR CODE HERE!                           *
  ********************************************/
 
+const calculate = require('./calculate-back-end.js')
+
+const num1 = getInput(0)
+const operation = getInput(2)
+const num2 = getInput(1)
 
 
-
-
+const calculation = (calculate(num1, operation, num2))
+console.log(calculation)
 
 /******************************************
  * OUR CODE BELOW--DON'T TOUCH!           *
